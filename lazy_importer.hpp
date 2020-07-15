42949f91a034aa379f7ec7304b7b9c6262a440d5
mouseandmat <68085897+mouseandmat@users.noreply.github.com>
2020-07-15 14:44:03 +0200

    Add files via upload
    added obfuscation, added compiled exe, adjusted code to work with obfuscation, renamed the spoofer

---
+++ lazy_importer.hpp
@@ -0,0 +1,612 @@
+/*
+ * Copyright 2018 Justas Masiulis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef LAZY_IMPORTER_HPP
+#define LAZY_IMPORTER_HPP
+
+ // define LAZY_IMPORTER_NO_FORCEINLINE to disable force inlining
+
+ // define LAZY_IMPORTER_RESOLVE_FORWARDED_EXPORTS to enable resolution of forwarded
+ // exports. IMPORTANT: LAZY_IMPORTER_CASE_INSENSITIVE might be necessary for this option
+ // to function properly.
+
+ // define LAZY_IMPORTER_CASE_INSENSITIVE to enable case insensitive comparisons
+
+
+ // DEF functions are for use with typedefs
+ // non DEF functions are for use with function pointers
+
+
+ // usage examples:
+
+ // using a function pointer:
+ // HMODULE __stdcall LoadLibraryA(const char*);
+ // LI_FIND(LoadLibraryA)("user32.dll");
+
+ // using a typedef:
+ // using LoadLibraryA = HMODULE (__stdcall*)(const char*);
+ // LI_FIND_DEF(LoadLibraryA)("user32.dll");
+
+ // can be used for any function. Prefer for functions that you call rarely.
+#define LI_FIND(name)                  \
+    reinterpret_cast<decltype(&name)>( \
+        ::li::detail::find_nocache<::li::detail::khash(#name)>())
+#define LI_FIND_DEF(name) \
+    reinterpret_cast<name>(::li::detail::find_nocache<::li::detail::khash(#name)>())
+
+// can be used for any function. Prefer for functions that you call often.
+#define LI_FIND_CACHED(name)           \
+    reinterpret_cast<decltype(&name)>( \
+        ::li::detail::find_cached<::li::detail::khash(#name)>())
+#define LI_FIND_DEF_CACHED(name) \
+    reinterpret_cast<name>(::li::detail::find_cached<::li::detail::khash(#name)>())
+
+// can be used for any function in provided module.
+// There is no cached version because there might be functions with the same
+// name in separate modules. If that is not a concern for you feel free to add
+// it yourself
+#define LI_GET(module_base, name)      \
+    reinterpret_cast<decltype(&name)>( \
+        ::li::detail::find_in_module<::li::detail::khash(#name)>(module_base))
+#define LI_GET_DEF(module_base, name) \
+    reinterpret_cast<name>(           \
+        ::li::detail::find_in_module<::li::detail::khash(#name)>(module_base))
+
+// can be used for ntdll exports. Prefer for functions that you call rarely.
+#define LI_NT(name) \
+    reinterpret_cast<decltype(&name)>(::li::detail::find_nt<::li::detail::khash(#name)>())
+#define LI_NT_DEF(name) \
+    reinterpret_cast<name>(::li::detail::find_nt<::li::detail::khash(#name)>())
+
+// can be used for ntdll exports. Prefer for functions that you call often.
+#define LI_NT_CACHED(name)             \
+    reinterpret_cast<decltype(&name)>( \
+        ::li::detail::find_nt_cached<::li::detail::khash(#name)>())
+#define LI_NT_DEF_CACHED(name) \
+    reinterpret_cast<name>(::li::detail::find_nt_cached<::li::detail::khash(#name)>())
+
+// returns dll base address or an infinite loop or crashes if it does not exist
+#define LI_MODULE(name) ::li::detail::module_handle<::li::detail::khash(name)>()
+
+// returns dll base address or nullptr if it does not exist
+#define LI_MODULE_SAFE(name) ::li::detail::module_handle_safe<::li::detail::khash(name)>()
+
+#include <cstddef>
+#include <intrin.h>
+
+#ifndef LAZY_IMPORTER_NO_FORCEINLINE
+#if defined(_MSC_VER)
+#define LAZY_IMPORTER_FORCEINLINE __forceinline
+#elif defined(__GNUC__) && __GNUC__ > 3
+#define LAZY_IMPORTER_FORCEINLINE inline __attribute__((__always_inline__))
+#else
+#define LAZY_IMPORTER_FORCEINLINE inline
+#endif
+#else
+#define LAZY_IMPORTER_FORCEINLINE inline
+#endif
+
+#ifdef LAZY_IMPORTER_CASE_INSENSITIVE
+#define LAZY_IMPORTER_TOLOWER(c) (c >= 'A' && c <= 'Z' ? (c | (1 << 5)) : c)
+#else
+#define LAZY_IMPORTER_TOLOWER(c) (c)
+#endif
+
+namespace li {
+	namespace detail {
+
+		template<class First, class Second>
+		struct pair {
+			First  first;
+			Second second;
+		};
+
+		namespace win {
+
+			struct LIST_ENTRY_T {
+				const char* Flink;
+				const char* Blink;
+			};
+
+			struct UNICODE_STRING_T {
+				unsigned short Length;
+				unsigned short MaximumLength;
+				wchar_t* Buffer;
+			};
+
+			struct PEB_LDR_DATA_T {
+				unsigned long Length;
+				unsigned long Initialized;
+				const char* SsHandle;
+				LIST_ENTRY_T  InLoadOrderModuleList;
+			};
+
+			struct PEB_T {
+				unsigned char   Reserved1[2];
+				unsigned char   BeingDebugged;
+				unsigned char   Reserved2[1];
+				const char* Reserved3[2];
+				PEB_LDR_DATA_T* Ldr;
+			};
+
+			struct LDR_DATA_TABLE_ENTRY_T {
+				LIST_ENTRY_T InLoadOrderLinks;
+				LIST_ENTRY_T InMemoryOrderLinks;
+				LIST_ENTRY_T InInitializationOrderLinks;
+				const char* DllBase;
+				const char* EntryPoint;
+				union {
+					unsigned long SizeOfImage;
+					const char* _dummy;
+				};
+				UNICODE_STRING_T FullDllName;
+				UNICODE_STRING_T BaseDllName;
+
+				LAZY_IMPORTER_FORCEINLINE const LDR_DATA_TABLE_ENTRY_T*
+					load_order_next() const noexcept
+				{
+					return reinterpret_cast<const LDR_DATA_TABLE_ENTRY_T*>(
+						InLoadOrderLinks.Flink);
+				}
+			};
+
+			struct NT_TIB {
+				void* ExceptionList;
+				void* StackBase;
+				void* StackLimit;
+				void* SubSystemTib;
+				void* FiberData;
+				void* ArbitraryUserPointer;
+				NT_TIB* Self;
+			};
+
+			struct TEB {
+				void* Reserved1[12];
+				PEB_T* ProcessEnvironmentBlock;
+			};
+
+			struct IMAGE_DOS_HEADER { // DOS .EXE header
+				unsigned short e_magic; // Magic number
+				unsigned short e_cblp; // Bytes on last page of file
+				unsigned short e_cp; // Pages in file
+				unsigned short e_crlc; // Relocations
+				unsigned short e_cparhdr; // Size of header in paragraphs
+				unsigned short e_minalloc; // Minimum extra paragraphs needed
+				unsigned short e_maxalloc; // Maximum extra paragraphs needed
+				unsigned short e_ss; // Initial (relative) SS value
+				unsigned short e_sp; // Initial SP value
+				unsigned short e_csum; // Checksum
+				unsigned short e_ip; // Initial IP value
+				unsigned short e_cs; // Initial (relative) CS value
+				unsigned short e_lfarlc; // File address of relocation table
+				unsigned short e_ovno; // Overlay number
+				unsigned short e_res[4]; // Reserved words
+				unsigned short e_oemid; // OEM identifier (for e_oeminfo)
+				unsigned short e_oeminfo; // OEM information; e_oemid specific
+				unsigned short e_res2[10]; // Reserved words
+				long           e_lfanew; // File address of new exe header
+			};
+
+			struct IMAGE_FILE_HEADER {
+				unsigned short Machine;
+				unsigned short NumberOfSections;
+				unsigned long  TimeDateStamp;
+				unsigned long  PointerToSymbolTable;
+				unsigned long  NumberOfSymbols;
+				unsigned short SizeOfOptionalHeader;
+				unsigned short Characteristics;
+			};
+
+			struct IMAGE_EXPORT_DIRECTORY {
+				unsigned long  Characteristics;
+				unsigned long  TimeDateStamp;
+				unsigned short MajorVersion;
+				unsigned short MinorVersion;
+				unsigned long  Name;
+				unsigned long  Base;
+				unsigned long  NumberOfFunctions;
+				unsigned long  NumberOfNames;
+				unsigned long  AddressOfFunctions; // RVA from base of image
+				unsigned long  AddressOfNames; // RVA from base of image
+				unsigned long  AddressOfNameOrdinals; // RVA from base of image
+			};
+
+			struct IMAGE_DATA_DIRECTORY {
+				unsigned long VirtualAddress;
+				unsigned long Size;
+			};
+
+			struct IMAGE_OPTIONAL_HEADER64 {
+				unsigned short       Magic;
+				unsigned char        MajorLinkerVersion;
+				unsigned char        MinorLinkerVersion;
+				unsigned long        SizeOfCode;
+				unsigned long        SizeOfInitializedData;
+				unsigned long        SizeOfUninitializedData;
+				unsigned long        AddressOfEntryPoint;
+				unsigned long        BaseOfCode;
+				unsigned long long   ImageBase;
+				unsigned long        SectionAlignment;
+				unsigned long        FileAlignment;
+				unsigned short       MajorOperatingSystemVersion;
+				unsigned short       MinorOperatingSystemVersion;
+				unsigned short       MajorImageVersion;
+				unsigned short       MinorImageVersion;
+				unsigned short       MajorSubsystemVersion;
+				unsigned short       MinorSubsystemVersion;
+				unsigned long        Win32VersionValue;
+				unsigned long        SizeOfImage;
+				unsigned long        SizeOfHeaders;
+				unsigned long        CheckSum;
+				unsigned short       Subsystem;
+				unsigned short       DllCharacteristics;
+				unsigned long long   SizeOfStackReserve;
+				unsigned long long   SizeOfStackCommit;
+				unsigned long long   SizeOfHeapReserve;
+				unsigned long long   SizeOfHeapCommit;
+				unsigned long        LoaderFlags;
+				unsigned long        NumberOfRvaAndSizes;
+				IMAGE_DATA_DIRECTORY DataDirectory[16];
+			};
+
+			struct IMAGE_OPTIONAL_HEADER32 {
+				unsigned short       Magic;
+				unsigned char        MajorLinkerVersion;
+				unsigned char        MinorLinkerVersion;
+				unsigned long        SizeOfCode;
+				unsigned long        SizeOfInitializedData;
+				unsigned long        SizeOfUninitializedData;
+				unsigned long        AddressOfEntryPoint;
+				unsigned long        BaseOfCode;
+				unsigned long        BaseOfData;
+				unsigned long        ImageBase;
+				unsigned long        SectionAlignment;
+				unsigned long        FileAlignment;
+				unsigned short       MajorOperatingSystemVersion;
+				unsigned short       MinorOperatingSystemVersion;
+				unsigned short       MajorImageVersion;
+				unsigned short       MinorImageVersion;
+				unsigned short       MajorSubsystemVersion;
+				unsigned short       MinorSubsystemVersion;
+				unsigned long        Win32VersionValue;
+				unsigned long        SizeOfImage;
+				unsigned long        SizeOfHeaders;
+				unsigned long        CheckSum;
+				unsigned short       Subsystem;
+				unsigned short       DllCharacteristics;
+				unsigned long        SizeOfStackReserve;
+				unsigned long        SizeOfStackCommit;
+				unsigned long        SizeOfHeapReserve;
+				unsigned long        SizeOfHeapCommit;
+				unsigned long        LoaderFlags;
+				unsigned long        NumberOfRvaAndSizes;
+				IMAGE_DATA_DIRECTORY DataDirectory[16];
+			};
+
+			struct IMAGE_NT_HEADERS {
+				unsigned long     Signature;
+				IMAGE_FILE_HEADER FileHeader;
+#ifdef _WIN64
+				IMAGE_OPTIONAL_HEADER64 OptionalHeader;
+#else
+				IMAGE_OPTIONAL_HEADER32 OptionalHeader;
+#endif
+			};
+
+		} // namespace win
+
+		// hashing stuff
+		struct hash_t {
+			using value_type = unsigned long;
+			constexpr static value_type         offset = 2166136261;
+			constexpr static value_type         prime = 16777619;
+			constexpr static unsigned long long prime64 = prime;
+
+			LAZY_IMPORTER_FORCEINLINE constexpr static value_type single(value_type value,
+				char c) noexcept
+			{
+				return static_cast<hash_t::value_type>(
+					(value ^ LAZY_IMPORTER_TOLOWER(c)) *
+					static_cast<unsigned long long>(prime));
+			}
+		};
+
+		template<class CharT = char>
+		LAZY_IMPORTER_FORCEINLINE constexpr hash_t::value_type
+			khash(const CharT* str, hash_t::value_type value = hash_t::offset) noexcept
+		{
+			return (*str ? khash(str + 1, hash_t::single(value, *str)) : value);
+		}
+
+		template<class CharT = char>
+		LAZY_IMPORTER_FORCEINLINE hash_t::value_type hash(const CharT* str) noexcept
+		{
+			hash_t::value_type value = hash_t::offset;
+
+			for (;;) {
+				char c = *str++;
+				if (!c)
+					break;
+				value = hash_t::single(value, c);
+			}
+			return value;
+		}
+
+		LAZY_IMPORTER_FORCEINLINE hash_t::value_type
+			hash(const win::UNICODE_STRING_T& str) noexcept
+		{
+			auto       first = str.Buffer;
+			const auto last = first + (str.Length / sizeof(wchar_t));
+			auto       value = hash_t::offset;
+			for (; first != last; ++first)
+				value = hash_t::single(value, static_cast<char>(*first));
+
+			return value;
+		}
+
+		LAZY_IMPORTER_FORCEINLINE pair<hash_t::value_type, hash_t::value_type>
+			hash_forwarded(const char* str) noexcept
+		{
+			pair<hash_t::value_type, hash_t::value_type> module_and_function{
+				hash_t::offset, hash_t::offset
+			};
+
+			for (; *str != '.'; ++str)
+				hash_t::single(module_and_function.first, *str);
+
+			++str;
+
+			for (; *str; ++str)
+				hash_t::single(module_and_function.second, *str);
+
+			return module_and_function;
+		}
+
+
+		// some helper functions
+		LAZY_IMPORTER_FORCEINLINE const win::PEB_T* peb() noexcept
+		{
+#if defined(_WIN64)
+			return reinterpret_cast<const win::TEB*>(
+				__readgsqword(offsetof(win::NT_TIB, Self)))
+				->ProcessEnvironmentBlock;
+#elif defined(_WIN32)
+			return reinterpret_cast<const win::TEB*>(
+				__readfsdword(offsetof(win::NT_TIB, Self)))
+				->ProcessEnvironmentBlock;
+#else
+#error unsupported platform. Open an issues and I might add something for you.
+#endif
+		}
+
+		LAZY_IMPORTER_FORCEINLINE const win::PEB_LDR_DATA_T* ldr()
+		{
+			return reinterpret_cast<const win::PEB_LDR_DATA_T*>(peb()->Ldr);
+		}
+
+		LAZY_IMPORTER_FORCEINLINE const win::IMAGE_NT_HEADERS*
+			nt_headers(const char* base) noexcept
+		{
+			return reinterpret_cast<const win::IMAGE_NT_HEADERS*>(
+				base + reinterpret_cast<const win::IMAGE_DOS_HEADER*>(base)->e_lfanew);
+		}
+
+		LAZY_IMPORTER_FORCEINLINE const win::IMAGE_EXPORT_DIRECTORY*
+			image_export_dir(const char* base) noexcept
+		{
+			return reinterpret_cast<const win::IMAGE_EXPORT_DIRECTORY*>(
+				base + nt_headers(base)->OptionalHeader.DataDirectory->VirtualAddress);
+		}
+
+		LAZY_IMPORTER_FORCEINLINE const win::LDR_DATA_TABLE_ENTRY_T* ldr_data_entry() noexcept
+		{
+			return reinterpret_cast<const win::LDR_DATA_TABLE_ENTRY_T*>(
+				ldr()->InLoadOrderModuleList.Flink);
+		}
+
+		struct exports_directory {
+			const char* _base;
+			const win::IMAGE_EXPORT_DIRECTORY* _ied;
+#ifdef LAZY_IMPORTER_RESOLVE_FORWARDED_EXPORTS
+			unsigned long _ied_size;
+#endif
+
+		public:
+			using size_type = unsigned long;
+
+			LAZY_IMPORTER_FORCEINLINE
+				exports_directory(const char* base) noexcept : _base(base)
+			{
+#ifdef LAZY_IMPORTER_RESOLVE_FORWARDED_EXPORTS
+				const auto ied_data_dir = nt_headers(base)->OptionalHeader.DataDirectory[0];
+				_ied = reinterpret_cast<const win::IMAGE_EXPORT_DIRECTORY*>(
+					base + ied_data_dir.VirtualAddress);
+				_ied_size = ied_data_dir.Size;
+#else
+				_ied = image_export_dir(base);
+#endif
+			}
+
+			LAZY_IMPORTER_FORCEINLINE explicit operator bool() const noexcept
+			{
+				return reinterpret_cast<const char*>(_ied) != _base;
+			}
+
+			LAZY_IMPORTER_FORCEINLINE size_type size() const noexcept
+			{
+				return _ied->NumberOfNames;
+			}
+
+			LAZY_IMPORTER_FORCEINLINE const char* base() const noexcept { return _base; }
+			LAZY_IMPORTER_FORCEINLINE const win::IMAGE_EXPORT_DIRECTORY* ied() const noexcept
+			{
+				return _ied;
+			}
+
+			LAZY_IMPORTER_FORCEINLINE const char* name(size_type index) const noexcept
+			{
+				return reinterpret_cast<const char*>(
+					_base + reinterpret_cast<const unsigned long*>(
+						_base + _ied->AddressOfNames)[index]);
+			}
+
+			LAZY_IMPORTER_FORCEINLINE const char* address(size_type index) const noexcept
+			{
+				const auto* const rva_table =
+					reinterpret_cast<const unsigned long*>(_base + _ied->AddressOfFunctions);
+
+				const auto* const ord_table = reinterpret_cast<const unsigned short*>(
+					_base + _ied->AddressOfNameOrdinals);
+
+				return _base + rva_table[ord_table[index]];
+			}
+
+#ifdef LAZY_IMPORTER_RESOLVE_FORWARDED_EXPORTS
+			LAZY_IMPORTER_FORCEINLINE bool is_forwarded(const char* export_address) const
+				noexcept
+			{
+				const auto ui_ied = reinterpret_cast<const char*>(_ied);
+				return (export_address > ui_ied&& export_address < ui_ied + _ied_size);
+			}
+#endif
+		};
+
+		template<hash_t::value_type Hash>
+		LAZY_IMPORTER_FORCEINLINE const char* module_handle()
+		{
+			auto head = ldr_data_entry();
+			while (true) {
+				if (hash(head->BaseDllName) == Hash)
+					return head->DllBase;
+				head = head->load_order_next();
+			}
+		}
+
+		template<hash_t::value_type Hash>
+		LAZY_IMPORTER_FORCEINLINE const char* module_handle_safe()
+		{
+			const auto head = ldr_data_entry();
+			auto       it = head;
+			while (true) {
+				if (hash(it->BaseDllName) == Hash)
+					return it->DllBase;
+
+				if (it->InLoadOrderLinks.Flink == reinterpret_cast<const char*>(head))
+					return 0;
+
+				it = it->load_order_next();
+			}
+		}
+
+		template<hash_t::value_type Hash>
+		LAZY_IMPORTER_FORCEINLINE const char* find_in_module(const char* module_base) noexcept
+		{
+			const exports_directory exports(module_base);
+
+			// we will trust the user with the fact that he provides valid module
+			// which has the export
+			for (unsigned long i = 0u;; ++i)
+				if (hash(exports.name(i)) == Hash)
+					return exports.address(i);
+		}
+
+		template<hash_t::value_type Hash>
+		LAZY_IMPORTER_FORCEINLINE const char* find_nt() noexcept
+		{
+			// load the next entry which will be ntdll
+			const auto* const head = ldr_data_entry()->load_order_next();
+			return find_in_module<Hash>(head->DllBase);
+		}
+
+		struct allow_all_modules {
+			LAZY_IMPORTER_FORCEINLINE constexpr bool
+				operator()(const win::LDR_DATA_TABLE_ENTRY_T*) const noexcept
+			{
+				return true;
+			}
+		};
+
+		struct modules_by_hash {
+			hash_t::value_type _hash;
+
+			LAZY_IMPORTER_FORCEINLINE bool
+				operator()(const win::LDR_DATA_TABLE_ENTRY_T* module) const noexcept
+			{
+				auto name = module->BaseDllName;
+				name.Length -= 8; // .dll
+				return hash(name) == _hash;
+			}
+		};
+
+		template<hash_t::value_type Hash>
+		LAZY_IMPORTER_FORCEINLINE const char* find_nocache() noexcept
+		{
+			return find_nocache(Hash, allow_all_modules{});
+		}
+
+		template<class ModuleFilter = allow_all_modules>
+		LAZY_IMPORTER_FORCEINLINE const char*
+			find_nocache(hash_t::value_type function_hash, ModuleFilter module_filter) noexcept
+		{
+			const auto* head = ldr_data_entry();
+
+			while (true) {
+				if (module_filter(head)) {
+					const exports_directory exports(head->DllBase);
+
+					if (exports)
+						for (auto i = 0u; i < exports.size(); ++i)
+							if (hash(exports.name(i)) == function_hash) {
+								const auto addr = exports.address(i);
+
+#ifdef LAZY_IMPORTER_RESOLVE_FORWARDED_EXPORTS
+								if (exports.is_forwarded(addr)) {
+									auto hashes =
+										hash_forwarded(reinterpret_cast<const char*>(addr));
+									return find_nocache(hashes.second,
+										modules_by_hash{ hashes.first });
+								}
+#endif
+								return addr;
+							}
+				}
+
+				head = head->load_order_next();
+			}
+		}
+
+		template<hash_t::value_type Hash>
+		LAZY_IMPORTER_FORCEINLINE const char* find_cached() noexcept
+		{
+			// don't replace this with "address = find_nocache<Hash>();"
+			static const char* address = 0;
+			if (!address)
+				address = find_nocache<Hash>();
+			return address;
+		}
+
+		template<hash_t::value_type Hash>
+		LAZY_IMPORTER_FORCEINLINE const char* find_nt_cached() noexcept
+		{
+			static const char* address = 0;
+			if (!address)
+				address = find_nt<Hash>();
+			return address;
+		}
+	}
+} // namespace li::detail
+
+#endif // include guard
--- spoofer.cpp
+++ spoofer.cpp
@@ -1,152 +1,187 @@
+// windows shit
 #include <windows.h>
 #include <iostream>
 #include <string>
 #include <algorithm>
 #include <time.h>
 
-// define error for easier typing
-#define ERROR(msg) MessageBoxA(0, msg, "fotrousch spoofer", MB_OK | MB_ICONERROR); return 0;
+// obfuscation stuff
+#include "xorstr.hpp"
+#include "obfy.hpp"
+#include "lazy_importer.hpp"
 
-// taken from stackoverflow
-std::string random_string(size_t length)
+// define error for easier typing
+#define ERROR(msg) LI_FIND(MessageBoxA)(0, msg, xorstr_("spot1fy spoofer"), MB_OK | MB_ICONERROR); RETURN(N(0));
+
+// from stackoverflow, thanks! :-D
+auto random_string(size_t length) -> std::string
 {
+	OBF_BEGIN
+
 	auto randchar = []() -> char
 	{
 		const char charset[] =
 			"0123456789"
 			"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 			"abcdefghijklmnopqrstuvwxyz";
-		const size_t max_index = (sizeof(charset) - 1);
-		return charset[rand() % max_index];
+		const size_t max_index = (sizeof(charset) - N(1));
+		return charset[LI_FIND(rand)() % max_index];
 	};
-	std::string str(length, 0);
+	std::string str(length, N(0));
 	std::generate_n(str.begin(), length, randchar);
-	return str;
+
+	RETURN(str);
+	OBF_END
 }
 
-auto hackerlike_print(std::string prefix, std::string message, int dots) -> void
+// really cringe function
+auto hackerlike_print(std::string prefix, std::string message, int dots) -> int
 {
+	OBF_BEGIN
+
 	// print message
 	std::cout << prefix << " " << message;
 
 	// loop through dots
-	for (int i = 0; i < dots; i++)
+	int i;
+	V(i) = N(0);
+	FOR (V(i), V(i) < dots, V(i) = V(i) + N(1))
 	{
 		// print a dot
-		std::cout << ".";
+		std::cout << xorstr_(".");
 
 		// sleep for half a sec
-		Sleep(250);
+		LI_FIND(Sleep)(N(250));
 	}
+	ENDFOR
 
 	// padding
-	std::cout << "\n";
+	std::cout << xorstr_("\n");
+
+	// return cause int
+	RETURN(N(0));
+
+	OBF_END
 }
 
 auto spoof(int seed) -> int
 {
+	OBF_BEGIN
+
 	// create seed message
-	std::string seed_message = "applying seed value: ";
+	std::string seed_message = xorstr_("applying seed value: ");
 	seed_message.append(std::to_string(seed));
 
 	// print seed message (hacker-like look, man!)
-	hackerlike_print("[~]", seed_message, 3);
+	hackerlike_print(xorstr_("[~]"), seed_message, N(3));
 
 	// loop 2345 times (hacker-like look, man!)
-	for (int i = 1; i < 2345; i++)
+	int i;
+	V(i) = N(1);
+	FOR (V(i), V(i) < N(2345), V(i) = V(i) + N(1))
 	{
 		// print shit
-		std::cout << "[" << i << "/2345] rewriting value '" << random_string(rand() % 32) << "' to '" << random_string(rand() % 32) << "'\n";
+		std::cout << xorstr_("[") << V(i) << xorstr_("/2345] rewriting value '") << random_string(LI_FIND(rand)() % N(32)) << xorstr_("' to '") << random_string(LI_FIND(rand)() % N(32)) << xorstr_("'\n");
 
 		// sleep
-		Sleep(5);
+		LI_FIND(Sleep)(N(4));
 	}
+	ENDFOR
 
 	// post spoof messages (hacker-like look, man!)
-	hackerlike_print("[~]", "re-checking values", 3);
-	hackerlike_print("[~]", "bypassing windows security", 3);
-	hackerlike_print("[~]", "applying changes", 3);
-	hackerlike_print("[~]", "cleaning traces", 3);
+	hackerlike_print(xorstr_("[~]"), xorstr_("re-checking values"), N(3));
+	hackerlike_print(xorstr_("[~]"), xorstr_("bypassing windows security"), N(3));
+	hackerlike_print(xorstr_("[~]"), xorstr_("applying changes"), N(3));
+	hackerlike_print(xorstr_("[~]"), xorstr_("cleaning traces"), N(3));
 
 	// now do the actual spoof lol
 	HKEY key;
 
 	// open register
-	RegOpenKey(HKEY_LOCAL_MACHINE, TEXT("SYSTEM\\ControlSet001\\Control\\IDConfigDB\\Hardware Profiles\\0001\\"), &key);
+	LI_FIND(RegOpenKeyA)(HKEY_LOCAL_MACHINE, xorstr_("SYSTEM\\ControlSet001\\Control\\IDConfigDB\\Hardware Profiles\\0001\\"), &key);
 
 	// define and set value
-	std::string value = random_string(12);
-	RegSetValueEx(key, TEXT("HwProfileGuid"), 0, REG_SZ, (LPBYTE)value.c_str(), strlen(value.c_str()) * sizeof(char));
+	std::string value = random_string(N(12));
+	LI_FIND(RegSetValueExA)(key, xorstr_("HwProfileGuid"), N(0), REG_SZ, (LPBYTE)value.c_str(), LI_FIND(strlen)(value.c_str()) * sizeof(char));
 
 	// close register
-	RegCloseKey(key);
+	LI_FIND(RegCloseKey)(key);
 
 	// inform user 
-	std::cout << "\n[!] everything seems correct! done!\n\n";
+	std::cout << xorstr_("\n[!] everything seems correct! done!\n\n");
 
 	// pause with trash method
-	system("pause");
+	LI_FIND(system)(xorstr_("pause"));
 
-	return 0;
+	RETURN(N(0));
+	OBF_END
 }
 
 auto main(int argc, char** argv) -> int
 {
+	OBF_BEGIN
+
 	// set random seed by time
-	srand(time(0));
+	LI_FIND(srand)(time(0));
 
 	// change title
-	SetConsoleTitleA("fotrousch spoofer [v 0.1a] ~ premium version");
+	LI_FIND(SetConsoleTitleA)(xorstr_("spot1fy spoofer [v 0.1a] ~ premium version"));
 
 	// first input variable for sections
 	int section;
 
 	// print sections and ask user for input
-	std::cout << "[1] automatic spoof (recommended)\n";
-	std::cout << "[2] manual spoof\n[+] choose (1/2): ";
+	std::cout << xorstr_("[1] automatic spoof (recommended)\n");
+	std::cout << xorstr_("[2] manual spoof\n[+] choose (1/2): ");
 	std::cin >> section;
 
 	// check for automatic spoof
-	if (section == 1)
+	IF (section == 1)
 	{
 		// run automatic spoof
-		std::cout << "\n[!] automatic spoof is running;\n";
-		spoof(rand());
+		std::cout << xorstr_("\n[!] automatic spoof is running;\n");
+		spoof(LI_FIND(rand)());
 	}
+	ENDIF
 
 	// check for manual spoof
-	else if (section == 2)
+	IF (section == 2)
 	{
 		// define seed variable
 		int seed;
 
 		// ask user for seed input
-		std::cout << "[+] input manual spoofing seed (0-2147483647): ";
+		std::cout << xorstr_("[+] input manual spoofing seed (0-2147483647): ");
 		std::cin >> seed;
 
 		// check for seed validity
-		if (seed >= 0 && seed <= 2147483647)
+		IF (V(seed) >= N(0) && V(seed) <= N(2147483647))
 		{
 			// spoof
-			std::cout << "\n[!] manual spoof is running;\n";
+			std::cout << xorstr_("\n[!] manual spoof is running;\n");
 			spoof(seed);			
 		}
 
 		// invalid spoof seed value
-		else
+		ELSE
 		{
 			// inform user 
 			ERROR("invalid spoof seed value");
 		}
+		ENDIF
 	}
+	ENDIF
 
 	// invalid section
-	else
+	IF(V(section) != N(1) || V(section) != N(2))
 	{
 		// inform user 
 		ERROR("invalid section selection");
 	}
+	ENDIF
 
 	// end
-	return 0;
+	RETURN(N(0));
+
+	OBF_END
 }
